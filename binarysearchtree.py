50875e0edd7e12eb3b6ec26bf896d7122e92e3b9
Drew Erny <drewerny@gmail.com>
2013-04-03 23:17:03 -0500

    fixed a file

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -9,10 +9,11 @@ class BinarySearchTree():
 
     def insert(self,value):
         node = Node(None,value,None)
-        if size == 0:
+        if self.size == 0:
             self._root = node
         else:
             self._place(node,self._root)
+            self.size += 1
         
 
     def _place(self,node,current):
@@ -29,8 +30,12 @@ class BinarySearchTree():
             else:
                 self._place(node,current.right)
 
-    def find(self,value):
-        walk = self._root
+    def find(self,value,node = None):
+        if node is None:
+            walk = self._root
+        else:
+            walk = node
+
         while walk is not None:
             if value == walk.value:
                 return walk
@@ -40,5 +45,45 @@ class BinarySearchTree():
                 walk = walk.left
         return None
 
-    def delete(self,value):
+    def delete(self,value,node = None):
+        if node is None:
+            node = self._root
+
+        #find the node to delete
+        node = find(value,node)
+        # if the node is a leaf
+        if node.left is None and node.right is None:
+            # just delete it (make it None)
+            node = None
+            return
+
+        #if one branch is none, link past the node to its subtree
+        elif node.left is None:
+            node = node.right
+            return
+        elif node.right is None:
+            node = node.left
+            return
+
+        #else the node has two branches
+        else:
+            node.value = findmax(node).value
+            self.delete(node.value,node.left)
+
+        self.size -= 1
+
+    def findmax(self,node = None):
+        if node is None:
+            node = self._root
+        while node.right is not None:
+            node = node.right
+        return node
+
+    def findmin(self,node = None):
+        if node is None:
+            node = self._root
+        while node.left is not None:
+            node = node.left 
+        return node
+
         
